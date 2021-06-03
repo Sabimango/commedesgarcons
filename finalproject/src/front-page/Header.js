d0f985e74fcc9acab9e5edda6eca3a67e5b77912
Adnan Dervisevic <adnan@adfenix.com>
2021-06-03 18:41:52 +0200

    Fixes

--- finalproject/src/front-page/Header.js
+++ finalproject/src/front-page/Header.js
@@ -1,20 +1,35 @@
-import "./index.css";
-import React from "react"
+import "../index.css";
 
-export const Header =()=> (
-    <header>
+import React from "react";
+
+export const Header = () => (
+  <header>
     <div className="container">
       <div className="navbar">
         <div className="logo">
-          <img src="images/comme-des-garcons-logo.png" width="175px" />
+          <img
+            src="images/comme-des-garcons-logo.png"
+            alt="logo"
+            width="175px"
+          />
         </div>
         <nav>
           <ul id="MenuItems">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="products.html">Products</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Contact</a></li>
-            <li><a href="">Gallery</a></li>
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li>
+              <a href="products.html">Products</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+            <li>
+              <a href="">Gallery</a>
+            </li>
           </ul>
         </nav>
         <i className="fas fa-shopping-cart"></i>
--- finalproject/src/front-page/Main-content.js
+++ finalproject/src/front-page/Main-content.js
@@ -1,65 +1,84 @@
-import "./index.css";
-import React from "react"
+import "../index.css";
 
-export const MainContent =()=> (
+import React from "react";
+
+export const MainContent = () => {
+  const itemsToFeature = [
+    { name: "BELTED TULLE MIDI DRESS", price: "775", currency: "€" },
+    { name: "LEATHER PLATFORM ANKLE BOOTS", price: "1,480", currency: "€" },
+    { name: "TULLE-TRIMMED MIDI DRESS", price: "1,225", currency: "€" },
+  ];
+
+  return (
     <div>
-<div className="row">
+      <div className="row">
         <div className="col-2">
           <h1>
             noir <br />
             kei ninomiya
           </h1>
+
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            <br />雨が降っている。 ブラウンさんをお願いします。
-          </p>
-          <a href="" className="btn">Discover SS21 Now</a>
-        </div>
-        <div className="col-2">
-          <img src="Images/front-image.jpg" />
-        </div>
-      </div>
-      <div className="categories">
-      <div className="mini-container">
-        <h2 className="title">FEATURED ITEMS</h2>
-        <div className="row">
-          <div className="col-3">
-            <img src="images/featured-1.jpg" alt="" />
-            <h4>BELTED TULLE MIDI DRESS</h4>
-            <p>€ 775</p>
-          </div>
-          <div className="col-3">
-            <img src="images/featured-2.jpg" alt="" />
-            <h4>LEATHER PLATFORM ANKLE BOOTS</h4>
-            <p>€ 1,480</p>
-          </div>
-          <div className="col-3">
-            <img src="images/featured-3.jpg" alt="" />
-            <h4>TULLE-TRIMMED MIDI DRESS</h4>
-            <p>€ 1,225</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="info">
-      <div className="mini-container">
-        <div className="row-1">
-          <div className="col-4">
-            <img src="images/info-picture.jpg" alt="" />
-          </div>
-          <div className="col-4">
-            <p>enter noir kei ninomiya vision</p>
-            <h1>runway since 1990</h1>
-            <small
-              >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
-              pariatur ratione quia eum quidem aperiam eius? 雨が降っている。
-              ブラウンさんをお願いします。</small
-            >
             <br />
-            <a href="" className="btn">Contact Us</a>
+            雨が降っている。 ブラウンさんをお願いします。
+          </p>
+
+          <a href="" className="btn">
+            Discover SS21 Now
+          </a>
+        </div>
+
+        <div className="col-2">
+          <img alt="front" src="Images/front-image.jpg" />
+        </div>
+      </div>
+
+      <div className="categories">
+        <div className="mini-container">
+          <h2 className="title">FEATURED ITEMS</h2>
+          <div className="row">
+            <div className="col-3">
+              {itemsToFeature.map((item, index) => (
+                <div style={{ marginBottom: 16 }}>
+                  <img
+                    src={`images/featured-${index + 1}.jpg`}
+                    alt={`${item.name}`}
+                  />
+                  <h4>{item.name}</h4>
+                  <p>
+                    {item.currency} {item.price}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="info">
+        <div className="mini-container">
+          <div className="row-1">
+            <div className="col-4">
+              <img src="images/info-picture.jpg" alt="" />
+            </div>
+            <div className="col-4">
+              <p>enter noir kei ninomiya vision</p>
+              <h1>runway since 1990</h1>
+              <small>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+                pariatur ratione quia eum quidem aperiam eius? 雨が降っている。
+                ブラウンさんをお願いします。
+              </small>
+
+              <br />
+
+              <a href="" className="btn">
+                Contact Us
+              </a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    </div>
-);
+  );
+};
